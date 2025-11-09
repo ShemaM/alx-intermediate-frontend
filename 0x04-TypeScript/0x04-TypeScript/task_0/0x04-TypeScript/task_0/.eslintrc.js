555564133bd295e7d80b48fd953036d5358d9e20
Shema Manasseh <shemamanase992@gmail.com>
2025-11-09 12:42:04 +0300

    added dependancies

---
+++ 0x04-TypeScript/0x04-TypeScript/task_0/0x04-TypeScript/task_0/.eslintrc.js
@@ -0,0 +1,12 @@
+﻿module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+  },
+};
